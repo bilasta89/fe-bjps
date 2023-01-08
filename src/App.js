194a4e9cb711762281b8843027eeb695da5581f7
bilasta89 <bilasta@gmail.com>
2023-01-08 20:01:35 +0700

    hapus login2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from '@components/login/login';
-import Login2 from '@components/login/login2';
 
 function App() {
   return (
